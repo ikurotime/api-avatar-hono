46ad13803c65b224ef1935f855401938fcad9d59
David Huertas <davidhuertasortiz@gmail.com>
2023-03-09 21:53:06 +0100

    feat: add skin tones and bg color

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,45 @@ const app = new Hono()
 
 const canvas = createCanvas(12, 12)
 const ctx = canvas.getContext('2d')
-
-ctx.fillStyle = '#D0FFD6'
-ctx.fillRect(0, 0, 12, 12)
-const img = await loadImage('img/template.png')
-ctx.drawImage(img, 0, 0, 12, 12)
-await Deno.writeFile('avatar.png', canvas.toBuffer())
-
+type Selector = {
+  [key: string]: string
+}
+const SKINS: Selector = {
+  1: 'img/skin/skin1.png',
+  2: 'img/skin/skin2.png',
+  3: 'img/skin/skin3.png'
+}
+const BG_COLORS: Selector = {
+  1: '#09f',
+  2: 'green',
+  3: 'red',
+  4: 'yellow',
+  5: '#f5f5f5'
+}
 app.use('*', logger())
 
-app.get('/', serveStatic({ path: 'avatar.png' }))
+app.get('/', async (c) => {
+  const { style, hair, skin, bg } = c.req.query()
+  // BG-COLOR
 
+  if (bg.length > 1) {
+    ctx.fillStyle = '#' + bg ?? '#161616'
+  } else {
+    ctx.fillStyle = BG_COLORS[bg] ?? '#161616'
+  }
+  ctx.fillRect(0, 0, 12, 12)
+  // SKIN
+  const skn = SKINS[skin] ?? 'template.png'
+
+  const skinImg = await loadImage(skn)
+  ctx.drawImage(skinImg, 0, 0, 12, 12)
+  await Deno.writeFile('avatar.png', canvas.toBuffer())
+  // HAIR
+  // STYLE
+  //console.log({ style, hair, skin, bg })
+  const resImg = await Deno.readFile('avatar.png')
+  return c.body(resImg)
+})
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) await serve(app.fetch)
 
