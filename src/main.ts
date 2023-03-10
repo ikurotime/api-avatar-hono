bfb96c539bfda9dfe3744b2c04571b0338ba4822
David Huertas <davidhuertasortiz@gmail.com>
2023-03-10 09:42:45 +0100

    fix: deno deploy writefile

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
-import { logger, serveStatic } from 'hono_middleware'
+import { logger } from 'hono_middleware'
 import { serve } from 'std/http/server.ts'
-import { createCanvas, loadImage } from 'https://deno.land/x/canvas/mod.ts'
+import {
+  createCanvas,
+  loadImage
+} from 'https://deno.land/x/canvas@v1.4.1/mod.ts'
 const app = new Hono()
 
 const canvas = createCanvas(12, 12)
@@ -26,7 +29,6 @@ app.use('*', logger())
 app.get('/', async (c) => {
   const { style, hair, skin, bg } = c.req.query()
   // BG-COLOR
-
   if (bg.length > 1) {
     ctx.fillStyle = '#' + bg ?? '#161616'
   } else {
@@ -35,14 +37,12 @@ app.get('/', async (c) => {
   ctx.fillRect(0, 0, 12, 12)
   // SKIN
   const skn = SKINS[skin] ?? 'template.png'
-
   const skinImg = await loadImage(skn)
   ctx.drawImage(skinImg, 0, 0, 12, 12)
-  await Deno.writeFile('avatar.png', canvas.toBuffer())
   // HAIR
   // STYLE
   //console.log({ style, hair, skin, bg })
-  const resImg = await Deno.readFile('avatar.png')
+  const resImg = canvas.toBuffer('image/png')
   return c.body(resImg)
 })
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
